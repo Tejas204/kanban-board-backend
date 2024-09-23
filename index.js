0b469315c80fe2d3c45586bf7682b711e4a86a64
Tejas204 <tejasdhopavkar2000@gmail.com>
2024-09-23 11:05:38 +0530

    Database connection established

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { name } from 'ejs';
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
 
 // Set up server
 const app = express();
@@ -12,8 +13,20 @@ app.use(express.urlencoded({extended: true}));
 // Set up View engine
 app.set("view engine", "ejs");
 
+//DB Connection
+mongoose.connect("mongodb://localhost:27017/", {
+    dbName : "SampleBackend"
+})
+.then(() => {console.log("DB Connected");})
+.catch((e) => console.log(e));
+
+//API
 app.get("/getProducts", (req, res) => {
     res.render("index", {name: "Tejas"})
+})
+
+app.get("/add", (req, res) => {
+    res.send("nice")
 })
 
 app.listen(5000, () => {
