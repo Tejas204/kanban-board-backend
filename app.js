f836f75407b1ab1dcd9de99f5ca0ec651213a666
Tejas204 <tejasdhopavkar2000@gmail.com>
2024-10-05 14:47:08 +0530

    Route splitting

--- app.js
+++ app.js
@@ -61,29 +61,7 @@ app.get("/login", (req, res) => {
     res.render("login");
 })
 
-app.get("/users/all", async (req, res) => {
-    const users = await User.find({});
-    console.log(users)
 
-    res.json({
-        success: "true",
-        users: users
-    })
-})
-
-app.get("/user/userid", async (req, res) => {
-    const token = req.cookies.token;
-
-    const loggedInUser = jwt.verify(token, "mySecret");
-        const loggedInUserId = loggedInUser._id;
-        const user = await User.findById({loggedInUserId});
-
-        res.json({
-            success: "true",
-            user: user
-        })
-    
-})
 
 
 // API: POST calls
