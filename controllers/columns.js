086dcfcf924052871f1235ea4b73f0298a289e23
Tejas204 <tejasdhopavkar2000@gmail.com>
2024-10-22 22:07:14 +0530

    Created column controller

--- controllers/columns.js
+++ controllers/columns.js
@@ -1,5 +1,6 @@
 import { Columns } from "../models/columns.js";
 
+// Get all columns
 export const fetchAllColumns = async (req,res) => {
     
     const columns = await Columns.find({});
@@ -8,4 +9,33 @@ export const fetchAllColumns = async (req,res) => {
         success: true,
         columns: columns
     });
+};
+
+// Create a new state
+export const createNewState = async (req, res) => {
+
+    const {name, user} = req.body;
+    // const token = req.cookies.token;
+    // console.log(token);
+    // const decoded = jwt.verify(token, "mySecret");
+    // const user = await User.findById(decoded._id);
+
+    // const columns = await Columns.findById({name});
+
+    // if(columns){
+    //     res.json({
+    //         success: false,
+    //         message: "Column with same name already exists"
+    //     });
+    // }
+    // else{
+    //     columns = await Columns.create({
+    //         name: name,
+    //         user: user
+    //     })
+    // }
+    const columns = await Columns.create({
+                name: name,
+                user: user
+            });
 };
--- routes/columns.js
+++ routes/columns.js
@@ -2,38 +2,12 @@ import express from "express";
 import { Columns } from "../models/columns.js";
 import { User } from "../models/user.js";
 import jwt from "jsonwebtoken";
-import { fetchAllColumns } from "../controllers/columns.js";
+import { createNewState, fetchAllColumns } from "../controllers/columns.js";
 
 const router = express.Router();
 
 router.get("/columns/allColumns", fetchAllColumns);
 
-router.post("/columns/createColumn", async (req, res) => {
-
-    const {name, user} = req.body;
-    // const token = req.cookies.token;
-    // console.log(token);
-    // const decoded = jwt.verify(token, "mySecret");
-    // const user = await User.findById(decoded._id);
-
-    // const columns = await Columns.findById({name});
-
-    // if(columns){
-    //     res.json({
-    //         success: false,
-    //         message: "Column with same name already exists"
-    //     });
-    // }
-    // else{
-    //     columns = await Columns.create({
-    //         name: name,
-    //         user: user
-    //     })
-    // }
-    const columns = await Columns.create({
-                name: name,
-                user: user
-            });
-});
+router.post("/columns/createColumn", createNewState);
 
 export default router;
