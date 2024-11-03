3433a2e0cbab55c83cfccd9ca43e706d7734eee1
Tejas204 <tejasdhopavkar2000@gmail.com>
2024-11-03 19:26:01 +0530

    Modified schema for states/columns

--- models/columns.js
+++ models/columns.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 
 const columnSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 export const Columns = mongoose.model("States", columnSchema);
