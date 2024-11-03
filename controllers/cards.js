3ade6d2e2e566b6307afb084ad825b8ffcbb4a01
Tejas204 <tejasdhopavkar2000@gmail.com>
2024-11-03 20:04:49 +0530

    Created schema for cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,23 +2,26 @@ import { Cards } from "../models/cards.js";
 import jwt from "jsonwebtoken";
 
 // Create a card
-export const createCard = async (req, res) => {
-  const { name, shortDescription, priority, state } = req.body;
-  const token = req.cookies.token;
+export const createCard = async (req, res, next) => {
+  try {
+    const { name, shortDescription, priority, state } = req.body;
+    const token = req.cookies.token;
 
-  const user = jwt.verify(token, process.env.JWT_SECRET);
-  console.log(user);
+    const user = jwt.verify(token, process.env.JWT_SECRET);
 
-  const card = await Cards.create({
-    name: name,
-    shortDescription: shortDescription,
-    assignedTo: user._id,
-    priority: priority,
-    state: state,
-  });
+    const card = await Cards.create({
+      name: name,
+      shortDescription: shortDescription,
+      assignedTo: user._id,
+      priority: priority,
+      state: state,
+    });
 
-  res.status(200).json({
-    success: true,
-    message: "Card created successfully",
-  });
+    res.status(200).json({
+      success: true,
+      message: "Card created successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
