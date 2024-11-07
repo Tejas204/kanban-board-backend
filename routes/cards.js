f10bc9e56fbd4df375d78247f626529267af1866
Tejas204 <tejasdhopavkar2000@gmail.com>
2024-11-07 10:31:51 +0530

    API to update card state

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
 import {
+  changeCardState,
   createCard,
   deleteCard,
   myCards,
@@ -24,5 +25,8 @@ router
   .route("/:id")
   .put(isAuthenticated, updateCard)
   .delete(isAuthenticated, deleteCard);
+
+// API: Update card state when card is dragged and dropped over a different column
+router.route("/changeCardState/:id").put(isAuthenticated, changeCardState);
 
 export default router;
