62124e7f194251e3c86011114ce0a31274ad9870
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-02-14 22:41:00 +0530

    Created Database: Comments

---
+++ models/comments.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const commentsSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export const Comments = mongoose.model("Comments", commentsSchema);
