8e3a4e983211502e25a4785cfe5c286d51a8112e
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-04-15 23:08:14 +0200

    API to create new kanban boards and fetch them

--- controllers/boards.js
+++ controllers/boards.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import { KanbanBoard } from "../models/boards.js";
+import ErrorHandler from "../middlewares/error.js";
 
 // Create a kanban board
 export const newKanbanBoard = async (req, res, next) => {
@@ -13,7 +14,28 @@ export const newKanbanBoard = async (req, res, next) => {
     });
 
     res.status(200).json({
-      message: "Kanban Board " + newKanbanBoard.name + " created successfully!",
+      message:
+        "Kanban Board: " + newKanbanBoard.name + " created successfully!",
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Fetch kanban boards created by logged in user
+export const getMyKanbanBoards = async (req, res, next) => {
+  try {
+    const user = req.user;
+
+    const myBoards = await KanbanBoard.find({ createdBy: user });
+
+    if (!myBoards) {
+      return next(new ErrorHandler("You don't have any kanban boards", 400));
+    }
+
+    res.status(200).json({
+      boards: myBoards,
       success: true,
     });
   } catch (error) {
--- routes/boards.js
+++ routes/boards.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { newKanbanBoard } from "../controllers/boards.js";
+import { getMyKanbanBoards, newKanbanBoard } from "../controllers/boards.js";
 
 // Define router
 const router = express.Router();
 
 // GET calls
+router.get("/getMyKanbanBoards", isAuthenticated, getMyKanbanBoards);
 
 // POST calls
 router.post("/newKanbanBoard", isAuthenticated, newKanbanBoard);
